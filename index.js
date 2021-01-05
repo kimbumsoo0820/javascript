a2eaab57fb98e0fe77369c60550b8e643a5a949e
kimbumsoo0820 <kbs2082@naver.com>
2021-01-05 21:38:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-console.log('Im working. Im JS. Im Beautiful. Im worth it');
-let a = 221;
-let b = a - 5;
-console.log(b, a);
+//String
+const what = "kbsss"
+console.log(what);
+
+//boolean
+const wat = false;
+console.log(wat);
+
+//float
+
+const waat = 55.55;
+console.log(waat);
