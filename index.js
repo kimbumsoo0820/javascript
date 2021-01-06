781722910d2437c9ec8101cb245ce8caf14eafc9
kimbumsoo0820 <kbs2082@naver.com>
2021-01-06 15:27:35 +0900

    3

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-function sayHello(name, age) {
-    return `Hello ${name} you are ${age} years old`;
-}
-
-const gg = sayHello("Kbs", 14);
-
-console.log(gg);
-
 
 const calculator = {
 
+    plus: function (a, b) {
+        return a + b;
+    },
+
+    min: function (a, b) {
+        return a - b;
+    }
 }
+
+const plus = calculator.plus(5, 5);
+
+console.log(plus);
+
+const min = calculator.min(10, 5);
+console.log(min);
