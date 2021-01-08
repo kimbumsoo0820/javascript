4994d7faca510fa6bceb5847f0dd37982d8b4a32
kimbumsoo0820 <kbs2082@naver.com>
2021-01-08 21:56:16 +0900

    practice2

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const title = document.querySelector("#title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    const hasClass = title.classList.contains(CLICKED_CLASS); // hasclass 가 CLICKED_CLASS를 갖고 있는지 판별함
-    if (hasClass) {
-        title.classList.remove(CLICKED_CLASS);
-    } else {
-        title.classList.add(CLICKED_CLASS);
-    }
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 
